eea7a81abd8a7906986e3129949ebecfded091f8
ppepin34 <ppepin34@gmail.com>
2022-03-05 21:09:38 -0500

    complete initial game

--- assets/js/game.js
+++ assets/js/game.js
@@ -70,8 +70,25 @@ var fight = function(enemyName) {
 
 // run fight function to start game
 for (var i = 0; i < enemyNames.length; i++) {
-  var pickedEnemyName = enemyNames[i];
-  enemyHealth = 50;
-  fight(pickedEnemyName);
+  if (playerHealth > 0) {
+    // let the player know what round they are in
+    window.alert("Welcome to Robot Gladiators! Round " + (i+1) );
+
+    //pick new enemy to fight based on the index of the enemyNames array
+    var pickedEnemyName = enemyNames[i];
+
+    //reset enemyHealth before starting new fight
+    enemyHealth = 50;  
+
+    //use debugger to pause script from running and check what's happening in the script
+    //debugger
+
+    //pass the pickedEnemyName variable's value into the fight function
+    fight(pickedEnemyName);
+  }
+  else {
+    window.alert("You have lost your robot in battle! Game Over!")
+    break;
+  }
 }
 
